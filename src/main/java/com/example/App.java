81b913508e5d82390e195f63eb1f84dabc9d83bc
sagar.dandgaval <sagardandgaval4@gmail.com>
2023-11-16 18:00:13 +0530

    converted into spring boot app for demo purpose

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,58 +1,14 @@
 package com.example;
 
-import org.apache.spark.SparkConf;
-import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import io.delta.tables.DeltaTable;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World New!" );
-        
-        // Set your Azure Storage account name and key
-        String azureClientId = "b33ce05c-63e0-42cb-9d75-6efe5a0d86df";
-        String azureClientSecret = "n+NZtxvNMmEAzq8sgnGGFz0if/0ZXQPZrmwqHjj3UZE=";
-        String azureStorageAccountName = "pltaxidatalake1";
-        String azureStorageAccountKey = "tt92YNKNv9SpcUnvjSHIsAdERGlS03wZnYJqZac2FklsT1vcUoG6T08+OSDC89YIWzTA/WNHLLo1+AStIj3KEQ==";
 
-        // Create a Spark configuration
-        SparkConf conf = new SparkConf().setAppName("PSDeltaLakeDemoRG");
-        SparkSession spark = SparkSession.builder().config(conf).config("spark.master", "local")
-            .config("fs.azure.account.auth.type.pltaxidatalake1.dfs.core.windows.net", "SharedKey")
-            .config("fs.azure.account.key.pltaxidatalake1.dfs.core.windows.net", azureStorageAccountKey)
-            .getOrCreate();
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication springBootApplication = new SpringApplication(App.class);
 
-        // // Set Azure Storage configuration
-        spark.conf().set("fs.azure.account.auth.type", "OAuth");
-        spark.conf().set("fs.azure.account.oauth.provider.type", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider");
-        spark.conf().set("fs.azure.account.auth.oauth2.client.id", azureClientId);
-        spark.conf().set("fs.azure.account.auth.oauth2.client.secret", azureClientSecret);
-        spark.conf().set("fs.azure.account.auth.oauth2.client.endpoint", "https://login.microsoftonline.com/6a310e33-5293-4125-87b1-0c69a570347f/oauth2/token");
-        spark.conf().set("spark.databricks.delta.preview.enabled", "true");
-        // Define the Azure Storage container and file path
-        String containerName = "taxidata";
-        String filePath = "Output/YellowTaxis.delta";
-
-        // Read data from Azure Storage into a DataFrame
-        String azurePath = "abfss://" + containerName + "@" + azureStorageAccountName + ".dfs.core.windows.net/" + filePath;
-        
-        System.out.println("**********************");
-        System.out.println(azurePath);
-        System.out.println("**********************");
-
-        Dataset<Row> data = spark.read().format("delta").load(azurePath).where("VendorId=3");
-        
-        data.show();
-
-        // Stop the Spark session
-        spark.stop();
-        
+        springBootApplication.run(args);
     }
 }
--- src/main/java/com/example/controller/DeltaSparkController.java
+++ src/main/java/com/example/controller/DeltaSparkController.java
@@ -1,6 +1,32 @@
 package com.example.controller;
 
 
+import com.example.services.ReadDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/spark")
 public class DeltaSparkController {
 
+    @Autowired
+    public DeltaSparkController() {
+    }
+
+    @Autowired
+    ReadDataService readDataService;
+
+    @RequestMapping(value = "/read/taxidata", method = RequestMethod.GET)
+    public List<String> getTaxiData() {
+        return readDataService.getDataFromContainer();
+    }
+
+    @RequestMapping(value = "/test")
+    public String apiTest() {
+        return "sagar api testing.......";
+    }
 }
